b4c57184bb12138adbb6530e5fa91888316e81fa
AnuBens <benjamin.lejeune@etu.estia.fr>
2024-12-03 13:59:57 +0000

    meteo_simusdata

--- main.cpp
+++ main.cpp
@@ -1,48 +1,69 @@
 #include "mbed.h"
 
+Thread thread_sensor;       
+Thread thread_led;          
+EventQueue event_queue;    
+DigitalOut led(LED1);     
+InterruptIn button(BUTTON1);  
 
-Thread thread_ping(osPriority::osPriorityAboveNormal);
-Thread thread_pong(osPriority::osPriorityBelowNormal);
-DigitalOut led(LED1);
-Mutex print_mutex; // Mutex pour protéger l'accès aux sorties console
+// SIMUS
+float get_temperature() { return 25.0f + rand() % 10; } 
+float get_humidity() { return 50.0f + rand() % 20; } 
+float get_pressure() { return 1013.25f + rand() % 10; }
 
 
-void ping()
+Mutex print_mutex;
+
+
+void read_sensor_data()
 {
     while (true) {
-        print_mutex.lock(); 
-        printf("Ping\n");
-        print_mutex.unlock(); 
-        ThisThread::sleep_for(100ms);
+        float temperature = get_temperature();
+        float humidity = get_humidity();
+
+        print_mutex.lock();
+        printf("Temperature : %.2f degres celsius\n", temperature);
+        printf("Humidite : %.2f %%\n", humidity);
+        print_mutex.unlock();
+
+        ThisThread::sleep_for(2s); 
     }
 }
 
 
-void pong()
+void display_pressure()
+{
+    float pressure = get_pressure();
+
+    print_mutex.lock();
+    printf("Pression atmospherique : %.2f hPa\n", pressure);
+    print_mutex.unlock();
+}
+
+
+void blink_led()
 {
     while (true) {
-        print_mutex.lock(); 
-        printf("Pong\n");
-        print_mutex.unlock(); 
-        ThisThread::sleep_for(100ms);
+        led = !led; 
+        ThisThread::sleep_for(5s);
     }
+}
+
+
+void schedule_pressure()
+{
+    event_queue.call(display_pressure);
 }
 
 int main()
 {
     
-
-
-
-    thread_ping.start(ping);
-    thread_pong.start(pong);
+    button.fall(schedule_pressure);
 
     
-    while (true) {
-        led = !led; 
-        print_mutex.lock(); 
-        printf("Alive!\n");
-        print_mutex.unlock(); 
-        ThisThread::sleep_for(500ms);
-    }
+    thread_sensor.start(read_sensor_data);
+    thread_led.start(blink_led);
+
+   
+    event_queue.dispatch_forever();
 }
